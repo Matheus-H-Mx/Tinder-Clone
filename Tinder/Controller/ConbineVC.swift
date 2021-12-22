18b45c2eb2ee4817ebb01596720d693dfd2b3385
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2021-12-22 19:31:50 -0300

    Personalizando o CombineCardView

--- Tinder/Controller/ConbineVC.swift
+++ Tinder/Controller/ConbineVC.swift
@@ -13,31 +13,47 @@ class CombineVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemGroupedBackground
     
-    let redView = UIView()
-        redView.backgroundColor = .red                  //cor da redview
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300) //transformando a redView em retangulo
-        redView.center = view.center                    //centralizaçao
-        
-        let gesture = UIPanGestureRecognizer()
-        gesture.addTarget(self, action: #selector(handlesCard))
-        
-        redView.addGestureRecognizer(gesture)
-        view.addSubview(redView)                        //chamada de view
-        
     
+        
+        self.adicionarCards()
     }
 
 
 }
+
+extension CombineVC {
+    func adicionarCards() {
+      
+        for item in 1...3 {
+            
+            let redView = UIView()
+            redView.backgroundColor = item == 2 ? .blue : .red                  //cor da redview
+            redView.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 33, height: view.bounds.height * 0.77) //transformando a redView em retangulo
+                redView.center = view.center                    //centralizaçao
+                
+                let gesture = UIPanGestureRecognizer()
+                gesture.addTarget(self, action: #selector(handlesCard))
+                
+                redView.addGestureRecognizer(gesture)
+                view.addSubview(redView)                        //chamada de view
+        }
+   
+    }
+}
+
 extension CombineVC {
     @objc func handlesCard(_ gesture: UIPanGestureRecognizer) { // identifica no console aonde o user esta clicando
         if let card = gesture.view {
             let point = gesture.translation(in: view)
             card.center = CGPoint (x: view.center.x + point.x, y: view.center.y + point.y)
             
+            let rotationAngle = point.x / view.bounds.width * 0.3
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
             if gesture.state == .ended {
-                UIView.animate(withDuration: 0.3) {
+                UIView.animate(withDuration: 0.3) {             //animação de volta ao centro ,apos o gesto acabar
                     card.center = self.view.center
+                    card.transform = .identity
                 }
             }
         }
---
+++ Tinder/View/CombineCardView.swift
@@ -0,0 +1,20 @@
+//
+//  CombineCardView.swift
+//  Tinder
+//
+//  Created by Matheus Henrique on 22/12/21.
+//
+
+import UIKit
+
+class CombineCardView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
