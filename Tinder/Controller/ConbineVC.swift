a57f3b834610e55bd02f7c6b837c081f2c78bf4e
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2021-12-26 23:45:46 -0300

    Ação de like e deslize e Verificar Match

--- Tinder/Controller/ConbineVC.swift
+++ Tinder/Controller/ConbineVC.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
+enum Acao {
+    case deslike
+    case superlike
+    case like
+}
+
 class CombineVC: UIViewController {
 
+    var perfilButton: UIButton = .iconMenu(named: "icone-perfil")
+    var chatButton: UIButton = .iconMenu(named: "icone-chat")
+    var logoButton: UIButton = .iconMenu(named: "icone-logo")
+    
+    var deslikeButton: UIButton = .iconFooter(named: "icone-deslike")       //chama as configs da lib arquivo UIButton
+    var superlikeButton: UIButton = .iconFooter(named: "icone-superlike")   //
+    var likeButton: UIButton = .iconFooter(named: "icone-like")             //
+    
     var usuarios: [Usuario] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
         view.backgroundColor = UIColor.systemGroupedBackground
-    
+        
+        self.adicionarHeader()
+        self.adicionarFooter()
         self.buscaUsuarios()
     }
     
     func buscaUsuarios () {
         self.usuarios = UsuarioService.shared.buscaUsuarios()
         self.adicionarCards()
-
     }
 
+}
+extension CombineVC {
+    func adicionarHeader () {
+        let window = UIApplication.shared.windows.first{ $0.isKeyWindow }
+        let top:CGFloat? = window?.safeAreaInsets.top ?? 44
+        
+        let stackView = UIStackView(arrangedSubviews: [perfilButton, logoButton, chatButton])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: nil,
+            padding: .init(top: top ?? <#default value#>!, left: 16, bottom: 0, right: 16)
+            
+        )
+    }
+    func adicionarFooter () {
+        let stackView = UIStackView(arrangedSubviews: [UIView(), deslikeButton, superlikeButton, likeButton,UIView()])
+        stackView.distribution = .equalCentering
+        
+        view.addSubview(stackView)
+        stackView.preencher(
+            top: nil,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: view.bottomAnchor,
+            padding: .init(top: 0, left: 16, bottom: 34, right: 16)
+        )
+            
+        deslikeButton.addTarget(self, action: #selector(deslikeClique), for: .touchUpInside)
+        superlikeButton.addTarget(self, action: #selector(superlikeClique), for: .touchUpInside)
+        likeButton.addTarget(self, action: #selector(likeClique), for: .touchUpInside)
+    }
 }
 
 extension CombineVC {
@@ -47,23 +100,123 @@ extension CombineVC {
         }
    
     }
+    func removerCard (card: UIView){
+        card.removeFromSuperview()
+        self.usuarios = self.usuarios.filter({(usuarios) -> Bool in
+            return usuarios.id != card.tag
+        })
+    }
+    func verificarMatch (usuario: Usuario){
+        if usuario.match {
+            print("wooow")
+        }
+        
+    }
 }
 
 extension CombineVC {
     @objc func handlesCard(_ gesture: UIPanGestureRecognizer) { // identifica no console aonde o user esta clicando
-        if let card = gesture.view {
+    
+        if let card = gesture.view as? CombineCardView{
             let point = gesture.translation(in: view)
+            
             card.center = CGPoint (x: view.center.x + point.x, y: view.center.y + point.y)
+             
+            let rotationAngle = point.x / view.bounds.width * 0.4     //rotação da animação usando o eixo x
             
-            let rotationAngle = point.x / view.bounds.width * 0.3       //rotação da animação usando o eixo x 
-            
+            if point.x > 0 {
+                card.likeIamgeView.alpha = rotationAngle * 5
+                card.deslikeIamgeView.alpha = 0
+            }  else {
+                card.likeIamgeView.alpha = 0
+                card.deslikeIamgeView.alpha = rotationAngle * 5-1;
+            }
+             
             card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+            
+            
             if gesture.state == .ended {
+                
+                if card.center.x > self.view.bounds.width + 50 {
+                    self.animarCard(rotationAmgle: rotationAngle, acao: .like)
+                    return
+                    //              print("like")
+                    //self.removerCard(card: card)   adiciona logica de remover o card quando LIKE
+                }
+                
+                if card.center.x < 0 - 50 {
+                    self.animarCard(rotationAmgle: rotationAngle, acao: .deslike)
+                    return
+                            //print("deslike")
+                    //self.removerCard(card: card) adiciona logica de remover o card quando DESLIKE
+                }
+                
                 UIView.animate(withDuration: 0.3) {             //animação de volta ao centro ,apos o gesto acabar
                     card.center = self.view.center
                     card.transform = .identity
+                    
+                    card.likeIamgeView.alpha = 0
+                    card.deslikeIamgeView.alpha = 0
+                }
+            }
+            
+        }
+    }
+    
+    @objc func deslikeClique() {
+        self.animarCard(rotationAmgle: -0.4, acao: .deslike)
+    }
+    @objc func likeClique() {
+        self.animarCard(rotationAmgle: 0.4, acao: .like)
+    }
+    @objc func superlikeClique() {
+        self.animarCard(rotationAmgle: 0.4, acao: .superlike)
+    }
+    
+    
+    
+    func animarCard (rotationAmgle: CGFloat, acao: Acao) {
+        if let usuario = self.usuarios.first {
+            for view in self.view.subviews{
+                if view.tag == usuario.id {
+                    if let card = view as? CombineCardView {
+                        let center: CGPoint
+                        var like: Bool
+                        
+                        switch acao {
+                        case .deslike:
+                            center = CGPoint(x: card.center.x - self.view.bounds.width, y: card.center.y + 50)
+                            like = false
+                        case .superlike:
+                            center = CGPoint(x: card.center.x, y: card.center.y - self.view.bounds.height)
+                            like = true
+                        case .like:
+                            center = CGPoint(x: card.center.x + self.view.bounds.width, y: card.center.y + 50)
+                            like = true
+                        }
+//                        UIView.animate(withDuration: 0.2){
+//                            card.center = center
+//                            card.transform = CGAffineTransform(rotationAngle: rotationAmgle)
+//                        }
+                        UIView.animate(withDuration: 0.2, animations: {
+                            card.center = center
+                            card.transform = CGAffineTransform(rotationAngle: rotationAmgle)
+                            
+                            card.deslikeIamgeView.alpha = like == false ? 1 : 0
+                            card.likeIamgeView.alpha = like == true ? 1 : 0
+                        }) { (_) in
+                            if like {
+                                self.verificarMatch(usuario: usuario)
+                            }
+                            
+                            self.removerCard(card: card)
+                    }
                 }
             }
         }
+        
+    }
+        
     }
 }
--- Tinder/View/CombineCardView.swift
+++ Tinder/View/CombineCardView.swift
@@ -20,19 +20,14 @@ class CombineCardView: UIView {
         }
     }
     
-    var fotoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "pessoa-1")    // comando q inicia com a image
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    
+    let fotoImageView: UIImageView = .fotoImageView()
     
     let nomeLabel: UILabel = .textBoldLabel(36, textColor: .white)
     let idadeLabel: UILabel = .textLabel(32, textColor: .white)
     let fraseLabel: UILabel = .textLabel(32, textColor: .white, numberOfLines: 3)
+    
+    let deslikeIamgeView: UIImageView = .iconCard(named: "card-deslike")        //icon com função pronta da UIImageVie
+    let likeIamgeView: UIImageView = .iconCard(named: "card-like")
 
     override init (frame: CGRect) {
         super.init(frame: frame)
@@ -41,15 +36,32 @@ class CombineCardView: UIView {
         layer.borderColor = UIColor.lightGray.cgColor    //cor da nossa color
         layer.cornerRadius = 8
         clipsToBounds = true
-        
-        nomeLabel.text = "Ana Julia"
-        idadeLabel.text = "18"
+    
         
         nomeLabel.adicionarShadow()
         idadeLabel.adicionarShadow()
         fraseLabel.adicionarShadow()
         
         addSubview(fotoImageView)
+        addSubview(deslikeImageView)
+        
+        deslikeImageView.preencher(
+            top: topAnchor,
+            leading: nil,
+            trailing: trailingAnchor,
+            bottom: nil,
+            padding: .init(top: 20, left: 0, bottom: 20, right: 0)
+        )
+        
+        addSubview(likeImageView),
+        likeImageView.preencher(
+            top: topAnchor,
+            leading: leadingAnchor,
+            trailing: nil,
+            bottom: nil,
+            padding: .init(top: 20, left: 20, bottom: 0, right: 0)
+        )
+        
         fotoImageView.preencherSuperview()
         
         let nomeIdadeStackView = UIStackView(arrangedSubviews: [nomeLabel, idadeLabel, UIView()])
